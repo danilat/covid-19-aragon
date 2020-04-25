93ce2cbb8f989153d46b1f50e11d264ada4f5d58
Dani Latorre <dani@danilat.com>
2020-04-25 18:04:53 +0200

    Improved the e2e test for aragon import

--- spec/updater_spec.rb
+++ spec/updater_spec.rb
@@ -1,16 +1,19 @@
 
 describe "Update data from aragon open data" do
   let(:source_file) { "./spec/support/sources/casos_coronavirus_aragon.csv" }
-  let(:expected_csv) { read_csv("./spec/support/outputs/coronavirus_cases.csv") }
+  let(:source) { read_csv(source_file) }
+  let(:target_file) { "./spec/support/outputs/coronavirus_cases.csv" }
+  let(:expected_target_rows) do
+    CSV.read(target_file, headers: true).collect do |row|
+      args = row.to_h.transform_keys!(&:to_sym)
+      TargetRow.new(args)
+    end
+  end
   subject(:process_daily_progression) { ProcessToDailyProgression.new }
 
   it "transdorm the input file to the output file" do
-    source = read_csv(source_file)
-
     target_rows = process_daily_progression.invoke(source, :aragon)
-    write_csv(target_rows, "tmp/coronavirus_cases.csv")
-    resultant_csv = read_csv("tmp/coronavirus_cases.csv")
     
-    expect(resultant_csv).to eq expected_csv
+    expect(target_rows).to eq expected_target_rows
   end
 end
