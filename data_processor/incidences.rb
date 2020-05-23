38f1a1c4c3b2c6e25b404531a91a504bda3ab625
Dani Latorre <dani@danilat.com>
2020-05-23 20:19:09 +0200

    Updating data with 23/05

--- data_processor/incidences.rb
+++ data_processor/incidences.rb
@@ -15,6 +15,7 @@ class Incidences
         "30/04/2020": 'El aumento en el número de casos detectados se corresponde con la inclusión en la estadística de 166 casos confirmados procedentes del brote asociado a dos centros de trabajo de Binéfar. Fuente: <a href="http://www.aragonhoy.net/index.php/mod.noticias/mem.detalle/area.1341/id.259460" target="_blank">Aragón Hoy</a>.',
         "05/05/2020": 'En Huesca se ha corregido el dato del día anterior, el número de fallecimientos acumuladas a día 05/05 pasa de 92 a 91.',
         "19/05/2020": 'Sanidad ha cambiado el criterio de contabilización de confirmados de forma que a partir del 18 solo se contabilizan como contagiados los confirmados a través de PCR y ELISA IgM. Este cambio de criterio provoca cifras no reales en las variaciones de casos activos, casos nuevos y altas.',
+        "23/05/2020": 'En Huesca se ha corregido el dato de fallecimientos, el número de fsallecimientos acumulados a día 23/05 pasa de 102 a 99.',
       },
       zaragoza: {
         "18/04/2020": 'El descuadre a nivel provincial se debe a que este día se han añadido los casos confirmados por test rápidos que se contabilizaron en Aragón el día 16. Fuente:  <a href="http://www.aragonhoy.net/index.php/mod.noticias/mem.detalle/area.1050/id.258841" target="_blank">Aragón Hoy</a>.',
@@ -26,6 +27,7 @@ class Incidences
         "20/04/2020": 'En Teruel se ha corregido el dato del día anterior, el número de altas acumuladas a día 20/04 pasa de 118 a 117.',
         "23/04/2020": 'Se ha modificado la cifra de las altas incluyendo también las altas epidemiológicas, es decir, las altas en casos confirmados que se han mantenido en su domicilio, que se suman a las hospitalarias. Fuente: <a href="http://www.aragonhoy.net/index.php/mod.noticias/mem.detalle/relmenu.9/id.259099" target="_blank">Aragón Hoy</a>.',
         "19/05/2020": 'Sanidad ha cambiado el criterio de contabilización de confirmados de forma que a partir del 18 solo se contabilizan como contagiados los confirmados a través de PCR y ELISA IgM. Este cambio de criterio provoca cifras no reales en las variaciones de casos activos, casos nuevos y altas.',
+        "23/05/2020": 'En Teruel se ha corregido el dato de nuevas altas, el número de altas acumuladas a día 23/05 pasa de 389 a 374.',
       },
       otros: {}
     }
